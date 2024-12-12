c430f8b7e793527e146555aece432eecbf8eb015
Jumar-Diaz <diazjumar5@gmail.com>
2024-12-12 17:12:05 -0500

    ajuste en el url agregando la vista de movil

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import recomendacion_view, lista_recomendaciones, save_calification, save_calification_image
+from .views import recomendacion_view, lista_recomendaciones, save_calification, save_calification_image, recomendacionMovil_view
 
 urlpatterns = [
     path('recomendacion/', recomendacion_view, name='recomendacion'),
+    path('recomendacionMovil/', recomendacionMovil_view, name = 'recomendacionMovil'),
     path('recomendaciones/', lista_recomendaciones, name='lista_recomendaciones'),
     path('calification/save', save_calification, name="guardarCalificacion"),
     path('calificationImage/saveImage', save_calification_image, name="guardarCalificationImage"),
